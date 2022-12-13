1d7f660068b9df473b97049d28e8ca5a39f94785
Lei Chen <leichen88@yahoo.fr>
2022-12-13 17:17:29 +0100

    small edits

--- assets/js/vendor/config.js
+++ assets/js/vendor/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/zjwzyjcl/cktkg0sli845w17p5gw16z9cl',
+    style: ['mapbox://styles/zjwzyjcl/cktkg0sli845w17p5gw16z9cl','mapbox://styles/zjwzyjcl/cksyl9ubw95cg17qinqporu7u'],
     accessToken: 'pk.eyJ1Ijoiemp3enlqY2wiLCJhIjoiY2xiZ2I1Z2kzMGVrODN2bnhzNXY1bHRhYSJ9.P8g6e_grpPgvTTG6buLDog',
     showMarkers: true,
     markerColor: '#3FB1CE',
@@ -14,13 +14,13 @@ var config = {
     subtitle: '',
     byline: '',
     footer: '',
-    chapters: [
+    chapters: [[
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
             title: 'Display Title',
-            image: './path/to/image/source.png',
+            image: '',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [-122.418398, 37.759483],
@@ -50,7 +50,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Second Title',
-            image: './path/to/image/source.png',
+            image: '',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [-77.020636, 38.886900],
@@ -75,7 +75,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Third Title',
-            image: './path/to/image/source.png',
+            image: '',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [6.15116, 46.20595],
@@ -94,7 +94,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Third Title',
-            image: './path/to/image/source.png',
+            image: '',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [-58.54195, -34.71600],
@@ -108,5 +108,102 @@ var config = {
             onChapterEnter: [],
             onChapterExit: []
         }
-    ]
+    ],
+    [
+        {
+            id: 'a',
+            alignment: 'center',
+            hidden: false,
+            title: 'Title 1',
+            image: '',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'b',
+            alignment: 'center',
+            hidden: false,
+            title: 'Title 2',
+            image: '',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'c',
+            alignment: 'center',
+            hidden: false,
+            title: 'Title 3',
+            image: '',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [6.15116, 46.20595],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'd',
+            alignment: 'center',
+            hidden: false,
+            title: 'Title 4',
+            image: '',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-58.54195, -34.71600],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]]
 };
+
+
